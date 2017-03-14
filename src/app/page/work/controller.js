f6f8662f7916be8eb695c534b840be8c9174c88d
Cptnsaftig <Chris.Kremer5@web.de>
2017-03-14 15:40:19 +0100

    Revert "FirstTest"
    This reverts commit 2a711063099fc65dd43a7214b9fae00e7f9ea197.

--- src/app/page/work/controller.js
+++ src/app/page/work/controller.js
@@ -21,8 +21,6 @@ module.exports = function (api) {
         mixins: [],
         data: data,
         ready () {
-	var menuItem = document.getElementById('menuwork');
-	menuItem.style.color = 'blue';
 
         },
         computed: {

e3ceefe0b61d518ceabb320bbf455f9348ab60fd
Cptnsaftig <Chris.Kremer5@web.de>
2018-02-08 18:25:56 +0100

    "

--- src/app/page/contact/controller.js
+++ src/app/page/contact/controller.js
@@ -62,7 +62,7 @@ module.exports = function (api) {
 				subject:    'Message Received ' + this.mail.subject,
 				text:       this.mail.text
 				};
-                return api('post', '/mail/', {}, dataMail, (err, result) => {
+                return api('post', '/mail/', dataMail, dataMail, (err, result) => {
                     this.removeError();
 					   this.success = true;
                     if (!err) {

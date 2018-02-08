7542fec7971e1670d58166112226f03adb751293
Cptnsaftig <Chris.Kremer5@web.de>
2018-02-08 21:38:16 +0100

    "

--- server/api/v1/service/mail.js
+++ server/api/v1/service/mail.js
@@ -19,13 +19,12 @@ module.exports = (app) => {
             var x = JSON.stringify(data);
 			var vars = x.split("##");
 			data.to = 'chris.kremer5@web.de';
-		/* 	data.from = vars[0].replace(/_([^_]*)$/,'.');
+ 
+			varsvars = vars[0].split('@');
+			data.from = varsvars[0] + varsvars[1].replace('_','.');
 			data.subject = vars[1];
 			data.text = vars[2];
-         */
-			data.from = "test@test1.de";
-			data.text = x;
-			data.subject = "tstyo";
+        
 		 
 		 
             mailgun.messages().send(data, cb);

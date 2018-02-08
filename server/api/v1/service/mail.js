ed16a394d137a88739de4a1e1a024d54f7fd14bf
Cptnsaftig <Chris.Kremer5@web.de>
2018-02-08 22:20:14 +0100

    "

--- server/api/v1/service/mail.js
+++ server/api/v1/service/mail.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
             // var x = JSON.stringify(data);
 			// var vars = x.split("##");
 			data.text = JSON.stringify(data);
-			 data.to = 'chris.kremer5@web.de';
+			 data.to = 'photography@feynsinn.design';
 			// data.from = vars[0];
 			// /* varsvars = vars[0].split('@');
 			// data.from = varsvars[0] + varsvars[1].replace('_','.'); */

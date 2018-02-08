0a2a858d31a18ec9be6ff567ab217bacde5992ff
Cptnsaftig <Chris.Kremer5@web.de>
2018-02-08 17:21:18 +0100

    "

--- config/service.js
+++ config/service.js
@@ -1,6 +1,6 @@
 module.exports = {
     mailgun: {
         api_key: process.env.MAILGUN_API_KEY || 'key-16447671c371614bbcb77d85cfa483e2',
-        domain: process.env.MAILGUN_DOMAIN || 'appcc6cb28d123b41ac9197c68fb4346628.mailgun.org'
+        domain: process.env.MAILGUN_DOMAIN || 'feynsinn.design'
     }
 }
